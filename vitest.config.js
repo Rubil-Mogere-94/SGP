e4be0da4f40cd3395b1201190a352dde23697e6f
Rubil <mogererubil@gmail.com>
2025-07-30 16:53:13 +0300

    new commit

--- vitest.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vitest/config';
-
-export default defineConfig({
-  test: {
-    environment: 'jsdom',
-    coverage: { provider: 'istanbul', reporter: ['text', 'html'] }
-  }
-});
